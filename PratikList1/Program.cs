3854e4c5363d72a5fe1e877c38816b90924fb27f
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-29 11:41:34 -0700

    Add project files.

---
+++ PratikList1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("** Davetliler **");
+List<string> list = new List<string>();//Creating generic list
+
+list.Add("Bulent Ersoy");//Adding element to list
+list.Add("Ajda Pekkan");
+list.Add("Ebru Gundes");
+list.Add("Hadise");
+list.Add("Hande Yener");
+list.Add("Tarkan");
+list.Add("Funda Arar");
+list.Add("Demet Akalin");
+
+foreach (string item in list)//Writing the elements of the list
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();
